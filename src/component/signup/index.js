8f13b4f0d8d9aa0a6f38869d03b44e7639f92d3d
kanishka-28 <kanishka.gour.mat20@itbhu.ac.in>
2021-11-13 12:36:00 +0530

    buttons changed

--- src/component/signup/index.js
+++ src/component/signup/index.js
@@ -1,6 +1,6 @@
-import { Fragment, useRef, useState, useEffect,useContext } from 'react'
+import { Fragment, useRef, useState, useEffect, useContext } from 'react'
 import { Dialog, Transition } from '@headlessui/react'
-import  { SignupContext} from "../../Context-Api/signup";
+import { SignupContext } from "../../Context-Api/signup";
 import { FcGoogle } from "react-icons/fc"
 import { AiOutlineClose } from "react-icons/ai";
 
@@ -8,59 +8,65 @@ import { AiOutlineClose } from "react-icons/ai";
 
 export default function Signup() {
   const [open, setOpen] = useContext(SignupContext);
-  
+
 
   const cancelButtonRef = useRef(null)
   const [status, setStatus] = useState("user");
   const [bgColor, setBgColor] = useState("gray-300")
+  const [userbtn, setuserbtn] = useState("megenta-400")
+  const [resbtn, setresbtn] = useState("megenta-400")
   const [cursor, setCursor] = useState("not-allowed")
   const [name, setName] = useState("")
   const [pass, setpass] = useState("")
-  const fName=(e)=>{
+  const fName = (e) => {
     setName(e.target.value)
-}
-  const fpass=(e)=>{
+  }
+  const fpass = (e) => {
     setpass(e.target.value)
   }
-  
 
-  const AsUser=()=>{
-    return(
+
+  const AsUser = () => {
+    return (
       <>
-      <form className="my-6">
-                          <input placeholder="Full Name" className="p-4 my-2 w-full h-12 focus:border-none focus:outline-none focus:ring-1 focus:ring-black  border border-gray-300 rounded-md" onChange={fName}/>
-                          <input placeholder="Password" className="p-4 my-2 w-full h-12 focus:border-none focus:outline-none focus:ring-1 focus:ring-black  border border-gray-300 rounded-md" onChange={fpass}/>
-                        </form>
+        <form className="my-6">
+          <input placeholder="Full Name" className="p-4 my-2 w-full h-12 focus:border-none focus:outline-none focus:ring-1 focus:ring-black  border border-gray-300 rounded-md" onChange={fName} />
+          <input placeholder="Password" className="p-4 my-2 w-full h-12 focus:border-none focus:outline-none focus:ring-1 focus:ring-black  border border-gray-300 rounded-md" onChange={fpass} />
+        </form>
       </>
-  
+
     )
   }
-  const AsRetauranant=()=>{
-    return(
+  const AsRetauranant = () => {
+    return (
       <>
-                          <form className="my-6">
-                          <input placeholder="Restaurant Name" className="p-4 my-2 w-full h-12 focus:border-none focus:outline-none focus:ring-1 focus:ring-black  border border-gray-300 rounded-md" onChange={fName}/>
-                          <input placeholder="pass" className="p-4 my-2 w-full h-12 focus:border-none focus:outline-none focus:ring-1 focus:ring-black  border border-gray-300 rounded-md" onChange={fpass}/>
-                        </form>
+        <form className="my-6">
+          <input placeholder="Restaurant Name" className="p-4 my-2 w-full h-12 focus:border-none focus:outline-none focus:ring-1 focus:ring-black  border border-gray-300 rounded-md" onChange={fName} />
+          <input placeholder="pass" className="p-4 my-2 w-full h-12 focus:border-none focus:outline-none focus:ring-1 focus:ring-black  border border-gray-300 rounded-md" onChange={fpass} />
+        </form>
       </>
-  
+
     )
   }
 
-  const handleSignUpAsUser=()=>{
+  const handleSignUpAsUser = () => {
     setStatus("user");
+    setuserbtn("megenta-500")
+    setresbtn("megenta-400")
   }
-  const handleSignUpAsRestaurant=()=>{
+  const handleSignUpAsRestaurant = () => {
     setStatus("restaurant");
+    setresbtn("megenta-500")
+    setuserbtn("megenta-400")
   }
 
   useEffect(() => {
-    if(name.length>5 && pass.length>10 && pass.includes("@")){
-        setBgColor("red-500")
-        setCursor("pointer")
-    }else{
-        setBgColor("gray-300")
-        setCursor("not-allowed")
+    if (name.length > 5 && pass.length > 10 && pass.includes("@")) {
+      setBgColor("red-500")
+      setCursor("pointer")
+    } else {
+      setBgColor("gray-300")
+      setCursor("not-allowed")
     }
   }, [name, pass])
 
@@ -100,31 +106,30 @@ export default function Signup() {
 
                   <div className="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left mb-8">
                     <div className="flex justify-between mb-8">
-                    <Dialog.Title as="h3" className="text-2xl leading-6 font-medium text-gray-900">
-                      Sign UP
-                    </Dialog.Title>
-                    <button type="button" className="text-red-500"
-                  onClick={() => setOpen(false)}
-                  ref={cancelButtonRef}><AiOutlineClose className="w-6"/></button>
+                      <Dialog.Title as="h3" className="text-2xl leading-6 font-medium text-gray-900">
+                        Sign UP
+                      </Dialog.Title>
+                      <button type="button" className="text-red-500"
+                        onClick={() => setOpen(false)}
+                        ref={cancelButtonRef}><AiOutlineClose className="w-6" /></button>
                     </div>
                     <div className="mt-2 text-center">
-                          <div className="flex justify-between">
-                            <button onClick={handleSignUpAsUser} className="bg-red-500 w-max md:w-1/3 rounded-lg p-4 text-white text-sm lg:text-lg" >Sign up as User</button>
-                            <button onClick={handleSignUpAsRestaurant} className="bg-red-500 w-max md:w-1/3 rounded-lg p-4 text-white text-sm lg:text-lg" >Sign up as Restaurant</button>
-                            </div>                  
-
+                      <div className="flex justify-between">
+                        <button onClick={handleSignUpAsUser} className={`bg-${userbtn} w-max md:w-1/3 rounded-lg p-4 text-white text-sm lg:text-lg hover:bg-megenta-500 focus:ring-2 focus:ring-offset-2 focus:ring-red-500 focus:border-megenta-300`} >Sign up as User</button>
+                        <button onClick={handleSignUpAsRestaurant} className={`bg-${resbtn} w-max md:w-1/3 rounded-lg p-4 text-white text-sm lg:text-lg hover:bg-megenta-500 focus:ring-2 focus:ring-offset-2 focus:ring-red-500 focus:border-megenta-300`}  >Sign up as Restaurant</button>
+                      </div>
                       {
-                        status==="user"&&
-                        <AsUser/>
+                        status === "user" &&
+                        <AsUser />
                       }
                       {
-                        status==="restaurant"&&
-                        <AsRetauranant/>
+                        status === "restaurant" &&
+                        <AsRetauranant />
                       }
                       <button className={`border border-gray-300 font-semibold w-full h-12 bg-${bgColor} cursor-${cursor}`}>Create Account</button>
                       <p className="m-4 font-dark text-xl">Or</p>
                       <div className="flex justify-center">
-                      <button className="w-full h-12 text-center text-semibold text-lg border-2 border-gray-300 py-1 bg-white flex justify-center items-center gap-2"><FcGoogle className="w-8 h-8"/><p>Continue With Google</p></button>
+                        <button className="w-full h-12 text-center text-semibold text-lg border-2 border-gray-300 py-1 bg-white flex justify-center items-center gap-2"><FcGoogle className="w-8 h-8" /><p>Continue With Google</p></button>
                       </div>
                     </div>
                   </div>
